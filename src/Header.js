7332f9cd252d73578df028423b4922179000d1f7
Jeffrey Platt <JSPlatt@gmail.com>
2021-05-24 12:55:46 -0400

    Header done

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton'
 import "./Header.css"
 
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
-            <h2>I am a header</h2>
-            <ForumIcon />
+            <IconButton> 
+                <PersonIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+
+            <img 
+                className= "header__logo" src="https://cdn.designrush.com/uploads/inspirations/2354/crop_683_410__1513706350_604_tin.png" alt="tinder logo"/>
+
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+
         </div>
     )
 }
